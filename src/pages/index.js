b54e319dd37d74323a845153d6ce2e3574b1121f
oscar.cardenas <oscar.cardenas@gse.com.co>
2023-05-02 16:02:22 -0500

    Rutas (Link) de Next

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-
+import Link from "next/link";
 export default function Home() {
   return (
     <>
-    
-      <h1>Hola en next</h1>
+      <h1>hola</h1>
+      <Link href="/nosotros">Nosotros</Link>
     </>
   );
 }
---
+++ src/pages/nosotros.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Nosotros() {
+  return (
+    <>
+      {" "}
+      <div>nosotros</div>
+      <Link href="/">Inicio</Link>
+    </>
+  );
+}
