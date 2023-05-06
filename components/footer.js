c114b2fd286afa0c03baa1ed0ce818446113d247
oscar.cardenas <oscar.cardenas@gse.com.co>
2023-05-06 17:11:40 -0500

    Css en el footer

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import styles from "../src/styles/footer.module.css";
 
 export default function Footer() {
   return (
-    <div>footer</div>
-  )
+    <footer className={styles.footer}>
+      <div className={`contenedor ${styles.contenido}`}>
+        <nav className={styles.navegacion}>
+          <Link href="/">Inicio</Link>
+          <Link href="/nosotros">Nosotros</Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/tienda">Tienda</Link>
+        </nav>
+        <p className={styles.copyright}>
+          Todos los derechos reservados {new Date().getFullYear()}
+        </p>
+      </div>
+    </footer>
+  );
 }
