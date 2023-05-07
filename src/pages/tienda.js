93390f6eb8d9bfd504b074317e84d3fc3fcfb1e4
oscar.cardenas <oscar.cardenas@gse.com.co>
2023-05-07 18:55:46 -0500

    Consultar API con getServerSideProps

--- src/pages/tienda.js
+++ src/pages/tienda.js
@@ -25,7 +25,27 @@ export default function Tienda({ guitarras }) {
  * the `guitarras` data fetched from the Strapi API. This data is being populated with the related
  * `imagen` data.
  */
-export async function getStaticProps() {
+// export async function getStaticProps() {
+//   const respuesta = await fetch(
+//     `${process.env.API_URL }/guitarras?populate=imagen`
+//   );
+//   const { data: guitarras } = await respuesta.json();
+
+//   //{gutarras} al exportarlo se puede pasar como props al componente tienda
+//   return {
+//     props: { guitarras },
+//   };
+// }
+
+/**
+ * This function fetches guitar data from an API and returns it as props for a component.
+ * @returns The `getServerSideProps` function is returning an object with a `props` property that
+ * contains an object with a `guitarras` property. The `guitarras` property is an array of guitar
+ * objects that were fetched from an API endpoint. This object can be passed as props to the `tienda`
+ * component.
+ */
+
+export async function getServerSideProps() {
   const respuesta = await fetch(
     `${process.env.API_URL }/guitarras?populate=imagen`
   );
